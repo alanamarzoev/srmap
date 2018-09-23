a85de8f442cb83ba894b8754397c8d1570c4b96d
Alana Marzoev <alanamarzoev@31-37-201.wireless.csail.mit.edu>
2018-09-23 16:58:33 -0400

    yikes

--- src/lib.rs
+++ src/lib.rs
@@ -1,37 +1,60 @@
+#![feature(trivial_bounds)]
 use std::collections::HashMap;
+use std::hash::Hash;
+use std::char;
+
 
 #[derive(Clone)]
-struct SRMap<K,V> {
-    g_map: HashMap<K,V>, // Global map
-    b_map: HashMap<K,Vec(bool)>, // Auxiliary bit map for global map
-    u_map: HashMap<String,V> // Universe specific map (used only when K,V conflict with g_map)
+pub(crate) struct SRMap<K, V>
+where
+    K: Eq + Hash + Clone,
+    std::string::String: std::convert::From<K>,
+    V: std::cmp::PartialEq + Clone,
+{
+    g_map: HashMap<K, V>, // Global map
+    b_map: HashMap<K, Vec<bool>>, // Auxiliary bit map for global map
+    u_map: HashMap<String, V>, // Universe specific map (used only when K,V conflict with g_map)
+    id_store: HashMap<usize, usize>,
+    largest: usize
 }
 
-impl SRMap<K,V> {
-    pub fn new() -> Self {
+impl<K, V> SRMap<K, V>
+where
+    K: Eq + Hash + Clone,
+    std::string::String: std::convert::From<K>,
+    V: std::cmp::PartialEq + Clone,
+{
+    pub(crate) fn new() -> Self {
         SRMap{
-            g_map: HashMap<K,V>::new(),
-            b_map: HashMap<K,Vec(bool)>::new()),
-            u_map: HashMap<String,V>::new()
+            g_map: HashMap::new(),
+            b_map: HashMap::new(),
+            u_map: HashMap::new(),
+            id_store: HashMap::new(),
+            largest: 0
         }
     }
 
-    pub fn insert(k: K, v: V, uid: u_size){
+    pub(crate) fn insert(&mut self, k: K, v: V, uid: usize){
         // check if record is in the global map
-        match self.g_map.get(&k) {
-            Some(&val) => {
+        match self.g_map.get_mut(&k) {
+            Some(val) => {
                 // if it *is* in the global map, and the values match, update access for this user
-                if val == v {
+                if *val == v {
                     // update flag in global bit map for this user
-                    match self.b_map.get(&k) {
-                        Some(&bitmap) => {
-                            bitmap[uid] = true;
+                    match self.b_map.get_mut(&k) {
+                        Some(mut bitmap) => {
+                            match self.id_store.get(&uid) {
+                                Some(&id) => {
+                                    bitmap[id] = true;
+                                },
+                                None => {}
+                            }
                         },
                         None => {}
                     }
                 } else {
                 // if v is different, insert (k,v) into umap as ('uid:k',v)
-                    let uid_str: String =  String::from(uid).to_owned();
+                    let uid_str = char::from_digit(uid as u32, 10).unwrap().to_string();
                     let k_str: String = String::from(k).to_owned();
                     let u_key = format!("{}{}", uid_str, k_str);
                     self.u_map.insert(u_key.clone(), v.clone());
@@ -39,28 +62,34 @@ impl SRMap<K,V> {
             },
             // add record to global map if it isn't already there
             None => {
-                self.g_map.insert(k.clone(), v.clone());
+
             }
         }
     }
 
-    pub fn get(k: K, uid: u_size) -> Option<V> {
-        let uid_str: String =  String::from(uid).to_owned();
-        let k_str: String = String::from(k).to_owned();
+    pub fn get(&mut self, k: K, uid: usize) -> Option<V> {
+        let uid_str = char::from_digit(uid as u32, 10).unwrap().to_string();
+        //let uid_str: String =  String::from(uid).to_owned();
+        let k_str: String = String::from(k.clone()).to_owned();
         let first_check = format!("{}{}", uid_str, k_str);
 
         match self.u_map.get(&first_check) {
-           Some(&val) => {Some(val)},
+           Some(val) => {Some(val.clone())},
            _ => {match self.g_map.get(&k) {
-                    Some(&g_val) => {
-                        match b_map.get(&k) {
-                            Some(&bitmap) => {
-                                let accessible = bitmap[uid];
-                                if accessible {
-                                    return g_val;
-                                }
-                                else {
-                                    return None;
+                    Some(g_val) => {
+                        match self.b_map.get_mut(&k) {
+                            Some(bitmap) => {
+                                match self.id_store.get(&uid) {
+                                    Some(&id) => {
+                                        let accessible = bitmap[id];
+                                        if accessible {
+                                            return Some(g_val.clone());
+                                        }
+                                        else {
+                                            return None;
+                                        }
+                                    },
+                                    None => {None}
                                 }
                             },
                             None => {
@@ -76,49 +105,59 @@ impl SRMap<K,V> {
          }
     }
 
-    pub fn remove(k: K, v:V, uid: u_size) {
-        let uid_str: String =  String::from(uid).to_owned();
-        let k_str: String = String::from(k).to_owned();
+    pub fn remove(&mut self, k: K, uid: usize) {
+        let uid_str = char::from_digit(uid as u32, 10).unwrap().to_string();
+        let k_str: String = String::from(k.clone()).to_owned();
         let first_check = format!("{}{}", uid_str, k_str);
+        let mut remove_entirely = true;
 
-        match self.u_map.get(&first_check) {
-            // if umap contains user specific key, just remove that
-            Some(&val) => {self.u_map.remove(first_check)},
-            // if no user specific version exists, remove user privileges for key from gmap.
-            // if no users have access to a key anymore, remove it entirely.
-            None => {
-                match self.b_map(&k){
-                    Some(&bitmap) => {
-                        bitmap[uid] = false;
-                        let mut remove_entirely = true;
-                        for pt in &bitmap {
-                            if pt {
-                                remove_entirely = false;
+        if self.u_map.contains_key(&first_check) {
+            self.u_map.remove(&first_check);
+        } else {
+            match self.b_map.get_mut(&k){
+                Some(bitmap) => {
+                    match self.id_store.get(&uid) {
+                        Some(&id) => {
+                            bitmap[id] = false;
+
+                            for pt in bitmap {
+                                if *pt {
+                                    remove_entirely = false;
+                                }
                             }
-                        }
-                        if remove_entirely {
-                            self.g_map.remove(&k);
-                            self.b_map.remove(&k);
-                        }
+                        },
+                        None => {}
                     }
-                    None => {}
                 }
+                None => {}
             }
         }
-    }
 
-    pub fn add_user(uid: u_size) {
-        // add bitmap flag for this user in every global bitmap
-        for (k, bmap) in &self.b_map {
-            let new_bmap = bmap.append(false); // TODO can you modify bmap in place?
-            self.b_map.insert(k, new_bmap);
+        if remove_entirely {
+            self.g_map.remove(&k);
+            self.b_map.remove(&k);
         }
     }
 
-    pub fn remove_user(k: K, uid: u_size) {
+    pub fn add_user(&mut self, uid: usize) {
+        let add = 1 as usize;
+        self.largest = self.largest + add;
+        self.id_store.insert(uid.clone(), self.largest.clone());
+        // add bitmap flag for this user in every global bitmap
+        for (_, bmap) in self.b_map.iter_mut() {
+            bmap.push(false);
+        }
+    }
+
+    pub fn remove_user(&mut self, uid: usize) {
         // remove all u_map records for this user and revoke access from all global entries
-        for (k, bmap) in &self.b_map {
-            bmap[uid] = false;
+        match self.id_store.get(&uid) {
+            Some(&id) => {
+                for (_, bmap) in self.b_map.iter_mut() {
+                    bmap[id] = false;
+                }
+            },
+            None => {}
         }
     }
 
